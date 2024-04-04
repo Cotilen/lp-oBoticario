cc8df64fcaf953621926d5fb0c9065994913d1aa
Cleiton <cleitonncruz105@gmail.com>
2024-04-04 18:34:59 -0300

    desktop pronto

---
+++ js/index.js
@@ -0,0 +1,30 @@
+const carouselSlide = document.querySelector('.carrossel');
+const cards = document.querySelectorAll('.produto');
+
+let counter = 0;
+const cardWidth = cards[0].clientWidth;
+
+function slide() {
+  carouselSlide.style.transform = `translateX(${-cardWidth * counter}px)`;
+}
+
+document.querySelector('.proximo-btn').addEventListener('click', () => {
+  if (counter >= cards.length - 1) return;
+  counter++;
+  slide();
+});
+
+document.querySelector('.anterior-btn').addEventListener('click', () => {
+  if (counter <= 0) return;
+  counter--;
+  slide();
+});
+
+function mudarEstado(elemento) {
+  let display = document.getElementById(elemento).style.display;
+  if (display == "flex") {
+      document.getElementById(elemento).style.display = "none";
+  } else {
+      document.getElementById(elemento).style.display = "flex";
+  }
+}
